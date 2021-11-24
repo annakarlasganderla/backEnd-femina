e21de65c5bca433615707ab85c726bf480f4e71b
anna sganderla <sganderlakarlaanna@gmail.com>
2021-11-24 19:45:39 -0300

    add menu

--- src/main/java/java/com/femina/produto/View/EndereçoView.java
+++ src/main/java/java/com/femina/produto/View/EndereçoView.java
@@ -125,7 +125,72 @@ public class EndereçoView {
         }
 
         System.out.println("Endereço deletado com sucesso!");
+    }
 
+    public void menuEndereco() {
+        int op = 0;
+        char control = 's';
+
+        do {
+            do {
+                System.out.println("-------------------------------------");
+                System.out.println("|        0 - Sair                   |");
+                System.out.println("|        1 - Cadastrar              |");
+                System.out.println("|        2 - Visualizar             |");
+                System.out.println("|        3 - Editar                 |");
+                System.out.println("|        4 - Deletar                |");
+                System.out.println("-------------------------------------");
+                System.out.println("|     Digite aqui a sua opção:      |");
+                System.out.println("-------------------------------------");
+                op = leitor.nextInt();
+            } while (op == 5);
+
+            switch (op) {
+
+                case 0:
+                    break;
+
+                case 1:
+                    do {
+                        this.cadastraEndereco();
+
+                        System.out.println("Deseja continuar cadastrando? digite s ou S para sim");
+                        control = leitor.next().charAt(0);
+
+                    } while (control == 's' || control == 'S');
+
+                    System.out.println("5 - Voltar");
+                    op = leitor.nextInt();
+                    break;
+
+                case 2:
+                    this.mostrarEnderecos();
+                    System.out.println("5 - Voltar");
+                    op = leitor.nextInt();
+                    break;
+
+                case 3:
+                    this.editarEndereco(selectEnderecoById());
+                    System.out.println("5 - Voltar");
+                    op = leitor.nextInt();
+                    break;
+
+                case 4:
+                    this.deletarEndereco(selectEnderecoById());
+                    System.out.println("5 - Voltar");
+                    op = leitor.nextInt();
+                    break;
+
+                default:
+                    System.out.println("Opção inválida");
+                    System.out.println("5 - Voltar");
+                    op = leitor.nextInt();
+                    break;
+            }
+
+        } while (op != 0);
+
+    }
     }
 
 }
