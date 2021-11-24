e83c013b53ee107a895c598d5cf8f2dddb714da7
VitorDanielAdams <vitor.adams@outlook.com>
2021-11-24 14:52:21 -0300

    001

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,6 @@
-package main;
-
-import main.java.com.femina.produto.Model.Cliente;
-import main.java.com.femina.produto.View.*;
-
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        MenuView menuView = new MenuView();
-        menuView.menuPrincipal();
+    public static void main(String[] args) {
+
     }
 }
