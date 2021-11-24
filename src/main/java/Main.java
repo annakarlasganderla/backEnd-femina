515c5b3f78e69791cf463adc1bac60dc821a3a9e
Maria Julia Furtado <mariajulia12003@gmail.com>
2021-11-24 20:26:07 -0300

    contatos check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
 package main;
 
-import main.java.com.femina.produto.Model.Cliente;
-import main.java.com.femina.produto.View.*;
+import main.java.com.femina.produto.View;
+
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        MenuView menuView = new MenuView();
+        menuView = new MenuView();
         menuView.menuPrincipal();
     }
 }
--- src/main/java/java/com/femina/produto/Dao/ContatoDao.java
+++ src/main/java/java/com/femina/produto/Dao/ContatoDao.java
@@ -1,110 +1,149 @@
-package main.java.com.femina.produto.Dao;
-import java.io.*;
+package Dao;
+
+import Factory.ConectionFactory;
+import Model.Contatos;
+
+import java.sql.*;
 import java.util.*;
-
-import main.java.com.femina.produto.Model.Contatos;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 
 public class ContatoDao {
 
+    private Connection connection;
 
-    public void cadastraContato(List<Contatos> contato,String arq) throws IOException {
-        File arquivo = new File("listaDeContatos"+arq+".txt");
+    public ContatoDao() {
+        this.connection = new ConectionFactory().getConection();
+    }
 
-        if(arquivo.isFile() ==  false){
-            arquivo.createNewFile();
+    public void criaTabela(){
+
+        String sql = "CREATE TABLE IF NOT EXISTS contatos ("+
+                "idContatos INT PRIMARY KEY AUTO_INCREMENT," +
+                "telefone VARCHAR(50) NOT NULL," +
+                "email VARCHAR(50) NOT NULL" +
+                ");";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+            stmt.execute();
+            stmt.close();
+
+            System.out.println("Tabela Contatos criada com sucesso!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+    }
 
-        FileWriter localDoArquivo = new FileWriter(arquivo, true);
-        PrintWriter escreveArquivo = new PrintWriter(localDoArquivo);
 
-        for (int i = 0;i < contato.size();i++) {
-            if(contato.get(i).getId() != Long.valueOf(i)+1) {
-                contato.get(i).setId(Long.valueOf(i)+1);
-                escreveArquivo.println(contato.get(i));
+    public void gravaNoBanco(Contatos contatos){
+
+        String sql = "INSERT INTO contatos" +
+                " (telefone,email) " +
+                "VALUES (?,?)";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setString(1, contatos.getTel());
+            stmt.setString(2, contatos.getEmail());
+
+            stmt.execute();
+
+            ResultSet resultSet = stmt.getGeneratedKeys();
+
+            while (resultSet.next()) {
+                contatos.setId(resultSet.getInt(1));
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        localDoArquivo.close();
-        escreveArquivo.flush();
-
     }
 
+    public List<Contatos> mostraLista(){
 
-    public List<Contatos> mostraContato(String arq) throws IOException {
+        String sql = "SELECT * FROM contatos";
 
-        File arquivo = new File("listaDeContatos"+arq+".txt");
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultSet = stmt.executeQuery();
 
-        if(arquivo.isFile() ==  false){
-            arquivo.createNewFile();
-        }
+            List<Contatos> contatos = new ArrayList<>();
+            Contatos contato;
 
-        FileReader arquivoTxt1 = new FileReader(arquivo);
-        BufferedReader lerArq = new BufferedReader(arquivoTxt1);
-
-        List<String> ListString = new ArrayList<>();
-        List<Contatos> listaDeContatos = new ArrayList<>();
-
-        String linha = "";
-
-        while ((linha = lerArq.readLine()) != null) {
-
-            if (linha != null) {
-                ListString.add(linha);
+            while (resultSet.next()) {
+                contato = new Contatos();
+                contato.setTel(resultSet.getString("telefone"));
+                contato.setEmail(resultSet.getString("email"));
+                contato.setId(resultSet.getInt("idContatos"));
+                contatos.add(contato);
             }
+
+            return contatos;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } }
+
+
+    public Contatos SelecionaId (int id){
+
+        String sql = "SELECT * FROM contatos WHERE idContatos = ?";
+
+        try {
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, id);
+
+            ResultSet resultSet = stmt.executeQuery();
+
+            while (resultSet.next()){
+                Contatos contato = new Contatos();
+                contato.setId(resultSet.getInt("idContatos"));
+                contato.setTel(resultSet.getString("telefone"));
+                contato.setEmail(resultSet.getString("email"));
+
+                return contato;
+            }
+
+        } catch (SQLException e){
+            throw new RuntimeException(e);
         }
 
-        arquivoTxt1.close();
-        lerArq.close();
+        return null;
+    }
 
-        for( String i : ListString){
+    public void editarContatoDoBanco(Contatos contatos){
+        String sql = "UPDATE contatos SET email = ?, telefone = ? WHERE idContatos = ?";
 
-            String[] prod = i.split(";");
-            Contatos contatos = new Contatos();
+        try {
 
-            contatos.setId(Long.valueOf(prod[0]));
-            contatos.setTel(prod[1]);
-            contatos.setEmail(prod[2]);
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, contatos.getEmail());
+            stmt.setString(2, contatos.getTel());
+            stmt.setInt(3, contatos.getId());
 
-            listaDeContatos.add(contatos);
+
+            stmt.execute();
+
+        } catch (SQLException e){
+            throw new RuntimeException(e);
         }
+    }
 
-        return listaDeContatos;
+    public void removeContatoDoBanco(Contatos contatos){
+        String sql = "DELETE FROM contatos WHERE idContatos = ?";
+
+        try {
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, contatos.getId());
+            stmt.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
-    public void editContatos(List <Contatos> contatos,String arq) throws IOException {
-
-        FileWriter arquivoTxt = new FileWriter("listaDeContatos"+arq+".txt",false);
-        PrintWriter gravaArq = new PrintWriter(arquivoTxt);
-
-        for (int l = 0; l < contatos.size();l++ ) {
-            gravaArq.println(contatos.get(l));
-        }
-
-        gravaArq.flush();
-        gravaArq.close();
-        arquivoTxt.close();
-
-    }
-
-    public void deletContatos (List<Contatos> contatos,String arq) throws IOException {
-
-        FileWriter arquivoTxt = new FileWriter("listaDeContatos"+arq+".txt",false);
-        PrintWriter gravaArq = new PrintWriter(arquivoTxt);
-
-        for (int l = 0; l < contatos.size();l++ ) {
-            contatos.get(l).setId(Long.valueOf(l)+1);
-            gravaArq.println(contatos.get(l));
-        }
-
-        gravaArq.flush();
-        gravaArq.close();
-        arquivoTxt.close();
-
-    }
 
 }
