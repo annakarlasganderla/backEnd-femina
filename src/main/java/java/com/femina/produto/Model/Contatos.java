7ee4eaa7aa2e96de834948633b74cc9e990b6d02
Anna Karla Sganderla <sganderlakarlaanna@gmail.com>
2021-11-25 19:23:12 -0300

    update contatos

--- src/main/java/java/com/femina/produto/Model/Contatos.java
+++ src/main/java/java/com/femina/produto/Model/Contatos.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Model;
 
 public class Contatos {
@@ -50,58 +49,3 @@ public class Contatos {
     }
 
 }
-
-=======
-//package java.com.femina.produto.Model;
-//
-//public class Contatos {
-//
-//    private long id;
-//    private String tel,email;
-//
-//    public Contatos(String tel, String email) {
-//        this.tel = tel;
-//        this.email = email;
-//    }
-//
-//    public Contatos() {
-//    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//
-//    public String getTel() {
-//        return tel;
-//    }
-//
-//    public void setTel(String tel) {
-//        this.tel = tel;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String toMostra() {
-//        return  "Contato: " +
-//                "Telefone - " + tel + "; " +
-//                "Email - " + email;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return id + ";" +  tel + ";" + email;
-//    }
-//
-//}
-//
->>>>>>> 2eef407d592c873b04e27a964b4054d98dbce436
