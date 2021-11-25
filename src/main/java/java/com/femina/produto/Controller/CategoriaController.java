4edea5aeac68cc7a112dff0e2c4daa255ab5e9aa
anna sganderla <sganderlakarlaanna@gmail.com>
2021-11-25 20:58:25 -0300

    add categorias

--- src/main/java/java/com/femina/produto/Controller/CategoriaController.java
+++ src/main/java/java/com/femina/produto/Controller/CategoriaController.java
@@ -1,34 +1,30 @@
-//package main.java.com.femina.produto.Controller;
-//import main.java.com.femina.produto.Dao.CategoriasDao;
-//import main.java.com.femina.produto.Model.Categoria;
-//
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class CategoriaController {
-//
-//    List<Categoria> categorias = new ArrayList<>();
-//    CategoriasDao categoriasDao = new CategoriasDao();
-//
-//    public void cadastrarCategoria(Categoria categoria) throws IOException {
-//        categorias.add(categoria);
-//        categoriasDao.cadastrarCategoria(categorias);
-//    }
-//
-//
-//    public List<Categoria> mostrarListaDeCategoria() throws IOException {
-//        categoriasDao.mostrarListaDeCategoria();
-//        return categorias;
-//    }
-//
-//    public void editaDeletaCategoria(List<Categoria> produtoDescontos) throws IOException {
-//        categoriasDao.editaDeletaCategorias(categorias);
-//    }
-//
-//    public void removeCategoria(List<Categoria> categorias) throws IOException {
-//        categoriasDao.editaDeletaCategorias(categorias);
-//    }
-//
-//
-//}
+package java.com.femina.produto.Controller;
+import java.com.femina.produto.Dao.CategoriaDao;
+import java.com.femina.produto.Model.Categoria;
+import java.util.List;
+
+public class CategoriaController {
+
+    CategoriaDao categoriaDao = new CategoriaDao();
+
+    public void criaTabelaCategoria(Categoria categoria) {
+        categoriaDao.criaTabelaCategoria(categoria);
+    }
+
+    public void cadastraCategoria(Categoria categoria) {
+        categoriaDao.cadastraCategoria(categoria);
+    }
+
+    public List<Categoria> listarCategorias() {
+        return categoriaDao.listarCategorias();
+    }
+
+    public Categoria selectById(int id) {
+        return categoriaDao.selectById(id);
+    }
+
+    public void deletarCategoria(Categoria categoria) {
+        categoriaDao.deletarCategoria(categoria);
+    }
+
+}
