a64d3c18018335aa46865fca6529d4e98f8076dd
VitorDanielAdams <vitor.adams@outlook.com>
2021-11-29 15:59:44 -0300

    002

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import java.com.femina.produto.View.ClienteView;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        ClienteView clienteView = new ClienteView();
+        clienteView.listarClientes();
     }
 }
