0ce700f46f98030d10592a49ca506d477ff3a461
anna sganderla <sganderlakarlaanna@gmail.com>
2021-12-01 21:35:03 -0300

    add menus

--- src/main/java/View/CargosView.java
+++ src/main/java/View/CargosView.java
@@ -30,4 +30,62 @@ public class CargosView {
             System.out.println((i+1) + " "+cargoController.listarCargos().get(i));
         }
     }
+
+    public void menuCargo() {
+        int op = 0;
+        char control = 's';
+
+        do {
+            do {
+                System.out.println("-------------------------------------");
+                System.out.println("|        0 - Sair                   |");
+                System.out.println("|        1 - Cadastrar              |");
+                System.out.println("|        2 - Visualizar             |");
+                System.out.println("|        3 - Deletar                |");
+                System.out.println("-------------------------------------");
+                System.out.println("|     Digite aqui a sua opção:      |");
+                System.out.println("-------------------------------------");
+                op = entrada.nextInt();
+            } while (op == 5);
+
+            switch (op) {
+
+                case 0:
+                    break;
+
+                case 1:
+                    do {
+                        this.cadastrarCargo();
+
+                        System.out.println("Deseja continuar cadastrando? digite s ou S para sim");
+                        control = entrada.next().charAt(0);
+
+                    } while (control == 's' || control == 'S');
+
+                    System.out.println("5 - Voltar");
+                    op = entrada.nextInt();
+                    break;
+
+                case 2:
+                    this.listarCargos();
+                    System.out.println("5 - Voltar");
+                    op = entrada.nextInt();
+                    break;
+
+                case 3:
+                    this.deletarCargo();
+                    System.out.println("5 - Voltar");
+                    op = entrada.nextInt();
+                    break;
+
+                default:
+                    System.out.println("Opção inválida");
+                    System.out.println("5 - Voltar");
+                    op = entrada.nextInt();
+                    break;
+            }
+
+        } while (op != 0);
+
+    }
 }
